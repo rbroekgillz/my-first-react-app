0f1a52203998652e89ed31c1b2a1f19a49e2ae5d
rbroekgillz <r.broek@gillz.nl>
2020-06-08 10:39:03 +0200

    Change some text on the App component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    return (
+        <div className="App">
+            {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,11 +16,24 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello world!
         </a>
-      </header>
-    </div>
-  );
+      </header>*/}
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>I love React!</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Click here to learn more
+                </a>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
